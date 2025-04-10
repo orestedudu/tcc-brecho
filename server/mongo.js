a6a2af84942d71988aeacd363036205fa030838f
jbgonca <you@example.com>
2025-04-10 20:16:27 -0300

    Criando db e collection

--- server/mongo.js
+++ server/mongo.js
@@ -16,16 +16,25 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server
     await client.connect();
+
     // Send a ping to confirm a successful connection
-    
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
+    console.log("Success in connection to MongoDB");
+
+    // Insert greetings in the MongoDB and print in console. 
+    const collection = client.db("test").collection('devices');
+    console.log(await collection.insertOne({ greeting: 'Hello Mongo' }));
+
+  } 
+  catch (error){
+    console.error("Error during connection: ", error)
+  }
+  finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 
 }
-run().catch(console.dir);
+run();
