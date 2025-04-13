60e310469a6a8a13408ad8c5790b9d510240d80a
jbgonca <you@example.com>
2025-04-13 15:25:03 -0300

    Instalei o cors e adicionei ele ao express

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 // Servidor Express que roda por cima do Node (servidor simples)
 
 const express = require('express');
+const cors = require('cors');
 const server = express();
 
+// Accept requests the others locations
+server.use(cors());
 server.get('/', (_, res) => {
-  res.send ('Hello EXPRESSSSSSSSSSSSS!');
+  res.send('Hello EXPRESSSSSSSSSSSSS!');
 });
 
 const port = 3001;
