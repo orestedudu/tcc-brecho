85b1940ccc2358c0ed9534bca426ee85dc3a4c7d
orestedudu <oresteeduardo81@gmail.com>
2025-09-05 16:03:37 -0300

    Categoria

--- server/models/categoria.js
+++ server/models/categoria.js
@@ -4,5 +4,5 @@ const categoriaSchema = new mongoose.Schema({
   admin: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true},
   nome: { type: String, required: true, trim: true, unique: true}, 
 }, { timestamps: true });
-
+//teste
 module.exports = mongoose.model("Categoria", categoriaSchema);
