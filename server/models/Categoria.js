506df84bc650fcbbe66519a5b6b30375b5af1de4
orestedudu <oresteeduardo81@gmail.com>
2025-09-05 16:10:13 -0300

    model categoria com C maiusculo

---
+++ server/models/Categoria.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const categoriaSchema = new mongoose.Schema({
+  admin: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true},
+  nome: { type: String, required: true, trim: true, unique: true}, 
+}, { timestamps: true });
+//teste
+module.exports = mongoose.model("Categoria", categoriaSchema);
