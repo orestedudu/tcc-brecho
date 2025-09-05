e14b56c7f3f05a7e715dc0440fbb797dd7bbb893
orestedudu <oresteeduardo81@gmail.com>
2025-09-05 16:08:02 -0300

    exclui o model categoria

--- server/models/categoria.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const categoriaSchema = new mongoose.Schema({
-  admin: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true},
-  nome: { type: String, required: true, trim: true, unique: true}, 
-}, { timestamps: true });
-//teste
-module.exports = mongoose.model("Categoria", categoriaSchema);
